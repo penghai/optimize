7c34aa6c6aa0d8c6abf92446a7851e25a11a1fe4
penghai <penghai_use@foxmail.com>
2019-09-09 14:32:57 +0800

    optimize

--- produce_hosts.sh
+++ produce_hosts.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-cidr=192.168.1.0/24
-startip=192.168.1.21
-endip=192.168.1.23
+cidr=192.168.99.0/24
+startip=192.168.99.221
+endip=192.168.99.223
 user=root
 password=centos
 subnet=`ipcalc -s -n $cidr|awk -F = '{print $2}'|awk -F 0 '{print $1}'`
